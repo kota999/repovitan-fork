0c9c7789276678bc46fb3fee4caf44a1914c98bd
joetheace107 <44982854+joetheace107@users.noreply.github.com>
2024-11-03 21:01:50 +0900

    next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class">{children}</ThemeProvider>{" "}
       </body>
     </html>
   );
