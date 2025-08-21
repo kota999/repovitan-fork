6055c37a62e48363ac29921bd28fac8bdbea85e8
Kota Natsume <kota99949@gmail.com>
2025-08-21 10:23:56 +0900

    for #8

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 px-8 py-4 pt-0">
-      <Link href="/share/bookmarks">Back to &quot;/share/bookmarks/&quot;</Link>
+      <Link href="/share/bookmarks">Go to &quot;/share/bookmarks/&quot;</Link>
     </div>
   );
 }
